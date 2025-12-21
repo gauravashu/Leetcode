a28d07587fad07e4ba3584ba080dd0225b41e974
Ashish Gaurav <ashish.gaurav64@gmail.com>
2025-12-21 13:16:52 +0530

    Implement minDeletionSize to count deletions

---
+++ 955.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int m = strs[0].length();
+        int n = strs.length;
+        boolean ordering[] = new boolean[n-1];
+        int delcount = 0;
+
+        for(int i=0; i<m; i++) {
+          
+            boolean delflag = false;
+            for(int j=0; j<n-1; j++) {
+                if(ordering[j]==false) {
+                    if(strs[j].charAt(i) > strs[j+1].charAt(i)) {
+                        delflag = true;
+                        break;
+                    }
+                }
+            }
+            if(delflag==true) {
+                delcount++;
+                continue;
+            }
+            
+            for(int k=0; k<n-1; k++) {
+                if(ordering[k]==false) {
+                    if(strs[k].charAt(i) < strs[k+1].charAt(i)) {
+                        ordering[k] = true;
+                    }
+                }
+            }
+        }
+        return delcount;
+
+    }
+}
+
+*/
