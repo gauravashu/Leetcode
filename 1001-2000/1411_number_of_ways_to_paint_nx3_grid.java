dcb20dbcf89debbae6b15788a2a7a7d4aeb73c0a
Ashish Gaurav <ashish.gaurav64@gmail.com>
2026-01-03 12:48:17 +0530

    Create 1411_number_of_ways_to_paint_nx3_grid.java

---
+++ 1001-2000/1411_number_of_ways_to_paint_nx3_grid.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    long MOD = 1_000_000_007L;
+    Long dp[][];
+    public int numOfWays(int n) {
+        dp = new Long[n][2];
+        long result =(6L*ways(1, n, 0))%MOD + (6L*ways(1, n , 1))%MOD;
+        return (int)(result%MOD);
+    }
+
+    long ways(int row, int n, int type) {
+        
+        if(row==n) {
+            return 1L;
+        }
+        if(dp[row][type]!=null) {
+            return dp[row][type];
+        }
+        long ans = 0;
+        if(type==0) {
+            ans = (2L*ways(row+1, n, type))%MOD + (2L*ways(row+1, n, 1))%MOD;
+        }
+        else if(type==1) {
+            ans =  (2L*ways(row+1, n, 0))%MOD + (3L*ways(row+1, n, type))%MOD;
+        }
+        dp[row][type] = ans%MOD;
+        return dp[row][type];
+    }
+}
+
+
